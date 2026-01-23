ce842a79f92630f44067c78354e67deafe215e00
Stevan H. Moeladi <stevan.moeladi@gmail.com>
2026-01-23 10:39:05 +0700

    fix: improve binance-kline node compliance
    - Fix Binance API auth to use X-MBX-APIKEY header
    - Add password protection to apiSecret field
    - Add documentationUrl to credentials
    - Add main field to package.json
    - Add icons directory for credential icons
    - Update docker-compose volume mount path

--- nodes/@stix/n8n-nodes-binance-kline/dist/credentials/BinanceApi.credentials.d.ts
+++ nodes/@stix/n8n-nodes-binance-kline/dist/credentials/BinanceApi.credentials.d.ts
@@ -1,7 +1,10 @@
-import { IAuthenticateGeneric, ICredentialTestRequest, ICredentialType, INodeProperties } from 'n8n-workflow';
+import { IAuthenticateGeneric, ICredentialTestRequest, ICredentialType, INodeProperties, Icon } from 'n8n-workflow';
 export declare class BinanceApi implements ICredentialType {
     name: string;
     displayName: string;
+    displayNamePlaceholder: string;
+    documentationUrl: string;
+    icon: Icon;
     properties: INodeProperties[];
     authenticate: IAuthenticateGeneric;
     test: ICredentialTestRequest;
